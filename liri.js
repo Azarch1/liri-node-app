925185ae4108f73ff24d96460d22fcf37fab54a0
SkynetDev <andrejohnson0017@gmail.com>
2019-04-20 23:24:33 -0400

    update

--- liri.js
+++ liri.js
@@ -1,1 +1,11 @@
 require("dotenv").config();
+var keys = require("./keys.js");
+
+var spotify = new Spotify(keys.spotify);
+var userInput = process.argv[2] 
+
+if (process.argv[2]==="spotify-this-song" || "concert-this" || "movie-this" || "do-what-it-says") {
+
+} else {
+    process.argv[2]===undefined
+}
